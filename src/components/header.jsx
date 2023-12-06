a857e8a0de23226a2741d7d60d47aa965283acf2
Joseph Dorman <JDorman555@yahoo.com>
2023-12-06 03:28:38 -0600

    make header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,19 @@
+import logo from '/logo.svg'
 import '../styles/header.css'
 
 function Header() {
   return (
     <>
-      <header>THIS IS A TEST</header>
+      <header>
+        <div className="logo"><button><img id="logo-img" src={logo}></img></button></div>
+        <nav>
+          <ul className='nav-buttons'>
+            <li><button>ABOUT</button></li>
+            <li><button>PROJECTS</button></li>
+            <li><button>CONTACT</button></li>
+          </ul>
+        </nav>
+      </header>
     </>
   )
 }
