87d0253e8b04c13d6096aa60feaaaa3f4c504787
Joseph Dorman <JDorman555@yahoo.com>
2023-12-06 04:03:00 -0600

    add hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,10 @@ function Hero() {
   return (
     <>
       <div className='hero'>
-        test
+        <div className="hero-container">
+          <h1 className='hero-title'>JOSEPH DORMAN</h1>
+          <p className='hero-desc'>Full Stack Developer</p>
+        </div>
       </div>
     </>
   )
