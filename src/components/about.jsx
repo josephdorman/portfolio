b949801e9145a11514518e6bb464f64e084e00de
Joseph Dorman <JDorman555@yahoo.com>
2023-12-07 01:46:42 -0600

    finish about me section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,6 +9,29 @@ function About() {
           <span>ABOUT ME</span>
         </h2>
         <p className='desc'>Hey, Im <b>Joseph</b>, a <b>Full-Stack Developer</b> currently based in <b>Texas</b>. Being a problem solver by heart and by trade, I love taking on <b>complex</b> and <b>challenging</b> projects. Im always open to <b>new</b> technologies, constantly <b>increasing</b> my experience with current and new tools available to me so I can <b>consistanly</b> create fast, responsive, and beatiful websites, <b>increasing</b> the user experience for <b>everyone</b>.</p>
+        <div className='carousel'>
+          <p>( Some Specialties )</p>
+          <div className="scroller">
+            <ul className="scroller-inner">
+              <li>REACT</li>
+              <li>NODEJS</li>
+              <li>JAVASCRIPT</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>UI/UX</li>
+              <li>GITHUB</li>
+              <li>MONGODB</li>
+              <li>REACT</li>
+              <li>NODEJS</li>
+              <li>JAVASCRIPT</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>UI/UX</li>
+              <li>GITHUB</li>
+              <li>MONGODB</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   )
