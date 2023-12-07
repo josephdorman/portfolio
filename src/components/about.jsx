8030d3a1a659d6ee36135f47777f3abb30366b12
Joseph Dorman <JDorman555@yahoo.com>
2023-12-07 01:18:01 -0600

    add to about me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,11 @@ function About() {
   return (
     <>
       <div className='about'>
-        <p className='title'>01. ABOUT</p>
+        <h2 className="title">
+          <p>( 01 )</p>
+          <span>ABOUT ME</span>
+        </h2>
+        <p className='desc'>Hey, Im <b>Joseph</b>, a <b>Full-Stack Developer</b> currently based in <b>Texas</b>. Being a problem solver by heart and by trade, I love taking on <b>complex</b> and <b>challenging</b> projects. Im always open to <b>new</b> technologies, constantly <b>increasing</b> my experience with current and new tools available to me so I can <b>consistanly</b> create fast, responsive, and beatiful websites, <b>increasing</b> the user experience for <b>everyone</b>.</p>
       </div>
     </>
   )
