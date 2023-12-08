30efab567429e4b8af7884f8cb26d3bb88a53a01
Joseph Dorman <JDorman555@yahoo.com>
2023-12-08 03:35:36 -0600

    fix: typo

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,7 +8,7 @@ function About() {
           <p>( 01 )</p>
           <span>ABOUT ME</span>
         </h2>
-        <p className='desc'>Hey, Im <b>Joseph</b>, a <b>Full-Stack Developer</b> currently based in <b>Texas</b>. Being a problem solver by heart and by trade, I love taking on <b>complex</b> and <b>challenging</b> projects. Im always open to <b>new</b> technologies, constantly <b>increasing</b> my experience with current and new tools available to me so I can <b>consistanly</b> create fast, responsive, and beatiful websites, <b>increasing</b> the user experience for <b>everyone</b>.</p>
+        <p className='desc'>Hey, Im <b>Joseph</b>, a <b>Full-Stack Developer</b> currently based in <b>Texas</b>. Being a problem solver by heart and by trade, I love taking on <b>complex</b> and <b>challenging</b> projects. Im always open to <b>new</b> technologies, constantly <b>increasing</b> my experience with current and new tools available to me so I can <b>consistanly</b> create fast, responsive, and beautiful websites, <b>increasing</b> the user experience for <b>everyone</b>.</p>
         <div className='carousel'>
           <p>( Some Specialties )</p>
           <div className="scroller">
