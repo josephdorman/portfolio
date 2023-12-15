5803fb8c1f6881abc8572dd128f6bb6cd92c6209
Joseph Dorman <JDorman555@yahoo.com>
2023-12-14 22:45:56 -0600

    style: add parralax effect

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,20 @@
+import { useRef } from "react";
 import lucid from "/lucidbot.jpg";
 import "../styles/projects.css";
 
 function Projects() {
+  const ref = useRef(null);
+
+  let centerX = window.innerWidth / 2;
+  let centerY = window.innerHeight / 2;
+
+  function onPointerMove(e) {
+    let diffX = (centerX - e.clientX) / 12;
+    let diffY = (centerY - e.clientY) / 12;
+
+    ref.current.style.transform = `translateY(${diffY}px) translateX(${diffX}px)`;
+  }
+
   return (
     <>
       <div className="projects">
@@ -10,8 +23,8 @@ function Projects() {
           <span>PROJECTS</span>
         </h2>
         <div className="project-container">
-          <div className="project-card lucid">
-            <img src={lucid} alt="" />
+          <div onPointerMove={onPointerMove} className="project-card lucid">
+            <img ref={ref} src={lucid} alt="" />
           </div>
         </div>
       </div>
