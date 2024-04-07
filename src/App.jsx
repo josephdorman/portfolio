d29587a959e6d8058996d8c00ec8219315f38512
Joseph Dorman <JDorman555@yahoo.com>
2024-04-06 23:35:46 -0500

    feat: nav to page section on click

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,34 @@ import Hero from "./components/hero";
 import About from "./components/about";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
+import { useRef } from "react";
 import "./styles/App.css";
 
 function App() {
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectRef = useRef(null);
+  const contactRef = useRef(null);
+
   return (
     <>
-      <div className="main">
+      <div ref={homeRef} className="main">
         <Rain />
-        <Header />
+        <Header
+          homeRef={homeRef}
+          aboutRef={aboutRef}
+          projectRef={projectRef}
+          contactRef={contactRef}
+        />
         <Hero />
       </div>
-      <section>
+      <section ref={aboutRef}>
         <About />
       </section>
-      <section>
+      <section ref={projectRef}>
         <Projects />
       </section>
-      <section>
+      <section ref={contactRef}>
         <Contact />
       </section>
       <footer>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,47 @@
-import logo from '/logo.svg'
-import '../styles/header.css'
+import logo from "/logo.svg";
+import "../styles/header.css";
 
-function Header() {
+function Header({ aboutRef, projectRef, contactRef, homeRef }) {
+  const scrollToHome = () => {
+    homeRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToAbout = () => {
+    aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToProject = () => {
+    projectRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToContact = () => {
+    contactRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <header>
-        <div className="logo"><button><img id="logo-img" src={logo}></img></button></div>
+        <div className="logo">
+          <button onClick={scrollToHome}>
+            <img id="logo-img" src={logo}></img>
+          </button>
+        </div>
         <nav>
-          <ul className='nav-buttons'>
-            <li><button>ABOUT</button></li>
-            <li><button>PROJECTS</button></li>
-            <li><button>CONTACT</button></li>
+          <ul className="nav-buttons">
+            <li>
+              <button onClick={scrollToAbout}>ABOUT</button>
+            </li>
+            <li>
+              <button onClick={scrollToProject}>PROJECTS</button>
+            </li>
+            <li>
+              <button onClick={scrollToContact}>CONTACT</button>
+            </li>
           </ul>
         </nav>
       </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
